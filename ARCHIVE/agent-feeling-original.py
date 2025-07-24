08546801dcaf07a524d032ea5f4fe2da170f8164
Devin Mason <95653073+masond84@users.noreply.github.com>
2025-07-24 16:29:42 -0400

    Update agent-feeling-original.py
    Remove hardcoded env secret keys

--- ARCHIVE/agent-feeling-original.py
+++ ARCHIVE/agent-feeling-original.py
@@ -14,8 +14,8 @@ import pyttsx3
 import threading
 
 # Setup API Keys
-os.environ["SERPAPI_API_KEY"] = "8d939898ac35a7185cbb7a8eb897c7105c197ad425cbd918cea6ba7386205ab6"
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["SERPAPI_API_KEY"] = SERPAPI_API_KEY
+os.environ["OPENAI_API_KEY"] = OPANAI_API_KEY
 
 # Create an instance of the OpenAI model
 llm = OpenAI(temperature=0)
